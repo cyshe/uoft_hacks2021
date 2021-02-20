17c564831b3698871a7654bab26794fa5fc22ac0
matthew.she <matthew.she@mail.utoronto.ca>
2021-02-20 11:54:30 -0500

    Added files for overall structure

---
+++ lib/Pages/ListPage.dart
@@ -0,0 +1,19 @@
+ import 'package:flutter/material.dart';
+
+class ListPage extends StatefulWidget {
+  ListPage({Key key}) : super(key: key);
+
+  @override
+  _ListPageState createState() => _ListPageState();
+}
+
+class _ListPageState extends State<ListPage> {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: Appbar(
+
+      )
+    )
+  }
+}
